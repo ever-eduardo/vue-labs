1632f18a24a1ebd68999a4b76552a40ead9efb54
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-27 12:49:12 -0500

    Use v-for concept for rendering lists.

--- app-intro/main.js
+++ app-intro/main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             url: 'https://vuejs.org/',
             inStock: true,
             inventory:100,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'greem'},
+                {id: 2235, color: 'blue'}
+            ],
+            sizes: ['S', 'M', 'L', 'XL']
         }
     }
 });
