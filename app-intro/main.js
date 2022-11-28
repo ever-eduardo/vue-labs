41209bde7392402031d105cff73ff0680a849ec6
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-27 21:16:54 -0500

    Add Vue components and props.

--- app-intro/main.js
+++ app-intro/main.js
@@ -2,41 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            url: 'https://vuejs.org/',
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
-            ],
-            sizes: ['S', 'M', 'L', 'XL']
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart++;
-        },
-        removeItem() {
-            this.cart--;
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        showSizes() {
-            return this.sizes.join(', ');
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity;
-        }
+
     }
 });
