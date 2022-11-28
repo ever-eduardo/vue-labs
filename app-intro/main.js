8c10be97f9210df22b0b81fc1ccd0055e67ea0e2
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-27 19:00:17 -0500

    Add computed properties.

--- app-intro/main.js
+++ app-intro/main.js
@@ -3,15 +3,13 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             url: 'https://vuejs.org/',
-            inStock: true,
-            inventory:100,
-            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
             ],
             sizes: ['S', 'M', 'L', 'XL']
         }
@@ -23,8 +21,22 @@ const app = Vue.createApp({
         removeItem() {
             this.cart--;
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        showSizes() {
+            return this.sizes.join(', ');
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
         }
     }
 });
