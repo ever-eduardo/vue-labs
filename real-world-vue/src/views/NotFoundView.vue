24704ab115eb0e39cc079926068e0b8b3f4b0d0a
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-29 10:56:38 -0500

    Add custom 404 component

---
+++ real-world-vue/src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Events for Good</h1>
+  <h2>404</h2>
+  <h2>Page not Found</h2>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 5rem;
+  text-align: center;
+  padding: 3rem;
+  margin: 3rem;
+}
+
+h2 {
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
