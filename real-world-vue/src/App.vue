1b5e6e34ae65f25476fb56fb138cf001fa2aec80
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-28 23:20:19 -0500

    Change hard coded routes to component names

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -6,8 +6,8 @@ import { RouterLink, RouterView } from "vue-router";
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Events</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{ name: 'EventList' }">Events</RouterLink>
+        <RouterLink :to="{ name: 'AboutView' }">About</RouterLink>
       </nav>
     </div>
   </header>
