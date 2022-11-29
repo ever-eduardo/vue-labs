1a95afc5983041dc75cb43eb7bd3dfab5f56897b
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-28 23:19:45 -0500

    Create EventDetails.vue

---
+++ real-world-vue/src/views/EventDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="event" class="detail">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeMount } from "vue";
+import EventService from "../services/EventService.js";
+const event = ref("");
+const props = defineProps(["id"]);
+
+onBeforeMount(() => {
+  EventService.getEvent(props.id)
+    .then((response) => {
+      event.value = response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+</script>
+
+<style scoped>
+.detail {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 2.5rem;
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
