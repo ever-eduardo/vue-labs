8e4069a2a164775d522d54a9bc08d6d717803c77
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-11-29 22:21:39 -0500

    Add NotFoundView.vue to handle error 404

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import EventDetails from "../views/EventDetails.vue";
+import NotFound from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: "EventDetails",
       component: EventDetails,
       props: true,
+    },
+    {
+      path: "/:pathMatch(.*)",
+      name: "NotFound",
+      component: NotFound,
     },
     {
       path: "/about",
--- real-world-vue/src/views/NotFoundView.vue
+++ real-world-vue/src/views/NotFoundView.vue
@@ -1,15 +1,15 @@
 <template>
   <h1>Events for Good</h1>
-  <h2>404</h2>
+  <h2>Error : 404</h2>
   <h2>Page not Found</h2>
 </template>
 
 <style scoped>
 h1 {
-  font-size: 5rem;
+  font-size: 3rem;
   text-align: center;
-  padding: 3rem;
-  margin: 3rem;
+  padding: 2rem;
+  margin: 2rem;
 }
 
 h2 {
