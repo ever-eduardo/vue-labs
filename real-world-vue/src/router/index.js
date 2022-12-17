5f2c21caf6583bf588076eccd9229be69d30f34e
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-12-16 20:20:42 -0500

    Upodated router with nested routes

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -3,6 +3,7 @@ import EventList from "../views/EventList.vue";
 import EventDetails from "../views/event/Details.vue";
 import EventRegister from "../views/event/Register.vue";
 import EventEdit from "../views/event/Edit.vue";
+import EventLayout from "../views/event/Layout.vue";
 import NotFound from "../views/NotFoundView.vue";
 
 const router = createRouter({
@@ -16,9 +17,26 @@ const router = createRouter({
     },
     {
       path: "/event/:id",
-      name: "EventDetails",
-      component: EventDetails,
+      name: "EventLayout",
+      component: EventLayout,
       props: true,
+      children: [
+        {
+          path: "",
+          name: "EventDetails",
+          component: EventDetails,
+        },
+        {
+          path: "register",
+          name: "EventRegister",
+          component: EventRegister,
+        },
+        {
+          path: "edit",
+          name: "EventEdit",
+          component: EventEdit,
+        },
+      ],
     },
     {
       path: "/:pathMatch(.*)",
