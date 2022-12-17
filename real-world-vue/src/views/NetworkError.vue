197645bac6090db916bc6690e89b18a8e2b9f15b
everness-dev <106024807+everness-dev@users.noreply.github.com>
2022-12-17 15:27:14 -0500

    Create NetworkError.vue

---
+++ real-world-vue/src/views/NetworkError.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Something went wrong :(</h2>
+  <h2>It looks like you are experiencing some network issues.</h2>
+  <h2>
+    Please, take a breath and
+    <a href="#" @click="router.go(-1)">click here</a> to try again.
+  </h2>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+</script>
